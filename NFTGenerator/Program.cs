f7b508db663b7f29460054ef670e52d45c3c0dd8
Matteo Beltrame <bltmtt@gmail.com>
2021-09-07 01:02:49 +0200

    Generation test

--- NFTGenerator/Program.cs
+++ NFTGenerator/Program.cs
@@ -7,17 +7,44 @@ namespace NFTGenerator
     internal class Program
     {
         private static List<Asset>[] layers;
+        private static Random random = new Random();
 
         private static void Main(string[] args)
         {
             Console.WriteLine("NFT Generator");
             LoadLayers();
+            Test();
+        }
+
+        private static void Test()
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                string resPath = "filesystem/results/res_" + j + ".gif";
+                List<Asset> toMerge = new List<Asset>();
+                for (int i = 0; i < layers.Length; i++)
+                {
+                    //TODO get random asset inside each layer
+                    toMerge.Add(layers[i][random.Next(0, layers[i].Count)]);
+                }
+                //At this point i have all the assets to be merged
+                if (toMerge.Count < 2)
+                {
+                    throw new Exception("Unable to merge less than 2 assets!");
+                }
+                // Create the first gif
+                GifHandler.MergeGifs(toMerge[0].AssetAbsolutePath, toMerge[1].AssetAbsolutePath, resPath);
+                for (int i = 2; i < toMerge.Count; i++)
+                {
+                    GifHandler.MergeGifs(resPath, toMerge[i].AssetAbsolutePath);
+                }
+            }
         }
 
         private static void LoadLayers()
         {
             Console.WriteLine("Loading layers");
-            string[] dirs = Directory.GetDirectories("filesystem");
+            string[] dirs = Directory.GetDirectories("filesystem/layers");
             layers = new List<Asset>[dirs.Length];
             for (int i = 0; i < dirs.Length; i++)
             {
