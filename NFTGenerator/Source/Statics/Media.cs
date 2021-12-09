acfc63f88790712d082aafe3483729f0be1c68a1
Matteo Beltrame <bltmtt@gmail.com>
2021-12-09 16:41:11 +0100

    Media composing fix

--- NFTGenerator/Source/Statics/Media.cs
+++ NFTGenerator/Source/Statics/Media.cs
@@ -2,7 +2,6 @@
 
 using HandierCli;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 
 namespace NFTGenerator;
@@ -11,17 +10,23 @@ internal static class Media
 {
     public static void ComposePNG(string res, Logger logger, params string[] pngs)
     {
-        var target = new Bitmap(1000, 1000);
-        using var composed = Graphics.FromImage(target);
-        //composed.CompositingMode = CompositingMode.SourceOver;
-        Rectangle rect = new Rectangle(0, 0, 1000, 1000);
+        Bitmap[] bitmaps = new Bitmap[pngs.Length];
         for (int i = 0; i < pngs.Length; i++)
         {
-            composed.DrawImage(new Bitmap(pngs[i]), rect);
-            //bitmaps[i].Dispose();
+            bitmaps[i] = new Bitmap(pngs[i]);
+        }
+        var width = bitmaps[0].Width;
+        var height = bitmaps[0].Height;
+        var target = new Bitmap(width, height);
+        using var composed = Graphics.FromImage(target);
+        Rectangle rect = new Rectangle(0, 0, width, height);
+        for (int i = 0; i < pngs.Length; i++)
+        {
+            composed.DrawImage(bitmaps[i], rect);
+            bitmaps[i].Dispose();
         }
         target.Save(res, ImageFormat.Png);
-        //target.Dispose();
-        //composed.Dispose();
+        target.Dispose();
+        composed.Dispose();
     }
 }
