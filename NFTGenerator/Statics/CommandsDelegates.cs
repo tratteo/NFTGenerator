36383d06be26cc7d79697d002ff9cdf6c56c6e95
Nicolò Madia <nclmda@gmail.com>
2022-04-01 17:05:14 +0200

    added glitch filter

---
+++ NFTGenerator/Statics/CommandsDelegates.cs
@@ -0,0 +1,473 @@
+﻿// Copyright Matteo Beltrame
+
+using BetterHaveIt;
+using HandierCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NFTGenerator.Metadata;
+using NFTGenerator.Objects;
+using NFTGenerator.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFTGenerator;
+
+internal static class CommandsDelegates
+{
+    public static void Verify(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        IConfiguration configuration = services.GetService<IConfiguration>();
+        string path = handler.GetPositional(0);
+        switch (path)
+        {
+            case "res":
+                var valid = true;
+                var assets = Directory.GetFiles($"{Paths.RESULTS}", "*.*").Where(s => s.EndsWith(".gif") || s.EndsWith(".jpeg") || s.EndsWith(".png")).ToArray();
+                if (assets.Length <= 0)
+                {
+                    logger.LogWarning("There is nothing in here");
+                    return;
+                }
+                var extension = string.Empty;
+                foreach (var asset in assets)
+                {
+                    FileInfo file = new(asset);
+                    if (!extension.Equals(string.Empty) && !extension.Equals(file.Extension))
+                    {
+                        logger.LogError("Found results with different extensions! This should never happen WTF");
+                        valid = false;
+                    }
+                }
+
+                var metadata = Directory.GetFiles($"{Paths.RESULTS}", "*.json");
+
+                if (assets.Length == metadata.Length && metadata.Length == 0)
+                {
+                    logger.LogWarning("There is nothing in here");
+                    return;
+                }
+                if (assets.Length != metadata.Length)
+                {
+                    logger.LogError("There are different numbers of assets and metadata. How the fuck did you manage to do such a thing");
+                    valid = false;
+                }
+                foreach (var data in metadata)
+                {
+                    if (Serializer.DeserializeJson<TokenMetadata>(string.Empty, data, out var nftData))
+                    {
+                        if (!nftData.Valid(logger))
+                        {
+                            logger.LogError($"Errors on metadata: {data}");
+                            logger.LogInformation("\n");
+                            valid = false;
+                        }
+                    }
+                }
+                if (valid)
+                {
+                    logger.LogInformation("All good in the results folder");
+                }
+                break;
+
+            case "fs":
+                IFilesystem filesystem = services.GetService<IFilesystem>();
+                filesystem.Verify();
+                break;
+        }
+    }
+
+    public static void PrepareBatch(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        List<(int index, Bitmap oldImage, TokenMetadata meta)> metadata = new List<(int, Bitmap, TokenMetadata)>();
+        string root = handler.GetPositional(0);
+        var metas = Directory.GetFiles(root, "*.json");
+        logger.LogInformation("Found {amount} metadata", metas.Length);
+        int count = 0;
+        for (var i = 300; i < 1569; i++, count++)
+        {
+            logger.LogInformation("Editing {d}", i);
+            var met = $"{root}\\{i}.json";
+            if (Serializer.DeserializeJson<TokenMetadata>(string.Empty, met, out var nftData))
+            {
+                if (!File.Exists($"{root}\\{nftData.Image}"))
+                {
+                    logger.LogError("Unable to find image asset");
+                    return;
+                }
+                foreach (var file in nftData.Properties.Files)
+                {
+                    file.Uri = $"{count}.png";
+                }
+                FileInfo imageInfo = new FileInfo($"{root}\\{nftData.Image}");
+                imageInfo.MoveTo($"{root}\\{count}.png", false);
+                nftData.Image = $"{count}.png";
+                Serializer.SerializeJson<TokenMetadata>(string.Empty, $"{root}\\{count}.json", nftData);
+                File.Delete(met);
+                //metadata.Add((i, new Bitmap($"{root}\\{nftData.Image}"), nftData));
+            }
+        }
+        //PurgeRecursive(root);
+        //foreach (var (index, oldImage, meta) in metadata)
+        //{
+        //    foreach (var file in meta.Properties.Files)
+        //    {
+        //        file.Uri = $"{index}.png";
+        //    }
+        //    meta.Image = $"{index}.png";
+        //    oldImage.Save($"{root}\\{index}.png");
+        //}
+        logger.LogInformation("Serialized {amount} metadata", metadata.Count);
+    }
+
+    public static void ApplyFilter(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        Media.Filter filter;
+        try
+        {
+            filter = (Media.Filter)Enum.Parse(typeof(Media.Filter), handler.GetPositional(2), true);
+        }
+        catch (Exception)
+        {
+            logger.LogError("Unable to parse filter");
+            return;
+        }
+        using Bitmap bitmap = new Bitmap(handler.GetPositional(0));
+        Media.ApplyFilter(bitmap, filter).Save(handler.GetPositional(1));
+    }
+
+    public static void RenameProgressively(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        int start = 0;
+        if (handler.GetKeyed("-si", out string startIndex))
+        {
+            try
+            {
+                start = int.Parse(startIndex);
+            }
+            catch (Exception)
+            {
+            }
+        }
+        int counter = start;
+        bool usingPattern = handler.GetKeyed("-pm", out var pattern);
+        var files = usingPattern ? Directory.GetFiles(handler.GetPositional(0), pattern) : Directory.GetFiles(handler.GetPositional(0));
+        var dirs = usingPattern ? Directory.GetDirectories(handler.GetPositional(0), pattern) : Directory.GetDirectories(handler.GetPositional(0));
+        for (int i = 0; i < dirs.Length; i++, counter++)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(dirs[i]);
+            (string path, string name) = PathExtensions.Split(directoryInfo.FullName);
+            if (directoryInfo.Name.Equals($"{counter}")) continue;
+            directoryInfo.MoveTo($"{path}\\{counter}");
+        }
+        for (int i = 0; i < files.Length; i++, counter++)
+        {
+            FileInfo fileInfo = new FileInfo(files[i]);
+            fileInfo.MoveTo($"{fileInfo.Directory.FullName}\\{counter}{fileInfo.Extension}", true);
+        }
+    }
+
+    public static void OpenPath(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        IConfiguration configuration = services.GetService<IConfiguration>();
+        IFilesystem filesystem = services.GetService<IFilesystem>();
+        switch (handler.GetPositional(0))
+        {
+            case "fs":
+                Process.Start("explorer.exe", $"{Paths.FILESYSTEM}");
+                break;
+
+            case "res":
+                Process.Start("explorer.exe", $"{Paths.RESULTS}");
+                break;
+
+            case "layer":
+                if (handler.GetKeyed("-n", out string layerNumber))
+                {
+                    if (int.TryParse(layerNumber, out int layerId) && layerId >= 0 && layerId < filesystem.Layers.Count)
+                    {
+                        Layer layer = filesystem.Layers[layerId];
+                        Process.Start("explorer.exe", $"{Paths.LAYERS}{layer.Name}");
+                    }
+                }
+                else
+                {
+                    Process.Start("explorer.exe", $"{Paths.LAYERS}");
+                }
+                break;
+
+            case "fallbacks":
+                Process.Start("explorer.exe", $"{Paths.FALLBACKS}");
+                break;
+
+            case "root":
+                Process.Start("explorer.exe", $"{Paths.ROOT}");
+                break;
+
+            default:
+                logger.LogWarning("Unable to find path");
+                break;
+        }
+    }
+
+    public static void PurgePath(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        IConfiguration configuration = services.GetService<IConfiguration>();
+        string path = handler.GetPositional(0);
+        bool force = handler.HasFlag("/f");
+        string answer;
+        switch (path)
+        {
+            case "res":
+                if (force)
+                {
+                    PurgeRecursive($"{Paths.RESULTS}", logger);
+                    logger.LogInformation("Purged results");
+                }
+                else
+                {
+                    logger.LogInformation("Are you sure you want to purge results folder? (Y/N)", ConsoleColor.DarkYellow);
+                    answer = Console.ReadLine();
+                    if (answer.ToLower().Equals("y"))
+                    {
+                        PurgeRecursive($"{Paths.RESULTS}", logger);
+                        logger.LogInformation("Purged results");
+                    }
+                }
+                break;
+
+            case "layers":
+                logger.LogInformation("Are you sure you want to purge layers? (Y/N)", ConsoleColor.DarkYellow);
+                answer = Console.ReadLine();
+                if (answer.ToLower().Equals("y"))
+                {
+                    PurgeRecursive($"{Paths.LAYERS}", logger);
+                    logger.LogInformation("Purged layers");
+                }
+                break;
+
+            case "fallbacks":
+                logger.LogInformation("Are you sure you want to purge fallbacks? (Y/N)", ConsoleColor.DarkYellow);
+                answer = Console.ReadLine();
+                if (answer.ToLower().Equals("y"))
+                {
+                    PurgeRecursive($"{Paths.FALLBACKS}", logger);
+                    logger.LogInformation("Purged fallbacks");
+                }
+                break;
+        }
+    }
+
+    public static void ScaleSerie(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        IConfiguration configuration = services.GetService<IConfiguration>();
+        IFilesystem filesystem = services.GetService<IFilesystem>();
+        logger.LogInformation("Are you sure you want to scale the serie number? (Y/N)", ConsoleColor.DarkYellow);
+        string answer = Console.ReadLine();
+        if (!answer.ToLower().Equals("y"))
+        {
+            return;
+        }
+        logger.LogInformation("It will not be possible to scale it back down, consider saving a copy of your filesystem, you want to proceed? (Y/N)", ConsoleColor.DarkYellow);
+        answer = Console.ReadLine();
+        if (!answer.ToLower().Equals("y"))
+        {
+            return;
+        }
+        if (!filesystem.Verify())
+        {
+            logger.LogError("Unable to scale, filesystem contains errors");
+            return;
+        }
+        else
+        {
+            int factor = 1;
+            try
+            {
+                factor = int.Parse(handler.GetPositional(0));
+                foreach (Layer layer in filesystem.Layers)
+                {
+                    foreach (Asset asset in layer.Assets)
+                    {
+                        asset.Metadata.Amount *= factor;
+                        Serializer.SerializeJson($"{Paths.LAYERS}{layer.Name}\\", $"{asset.Id}.json", asset.Metadata);
+                    }
+                }
+                configuration["Generation:SerieCount"] = (configuration.GetValue<int>("Generation:SerieCount") * factor).ToString();
+            }
+            catch (Exception)
+            {
+                logger.LogError("Unable to parse int factor");
+                return;
+            }
+        }
+    }
+
+    public static async Task Generate(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        IConfiguration configuration = services.GetService<IConfiguration>();
+        IFilesystem filesystem = services.GetService<IFilesystem>();
+        IGenerator generator = services.GetService<IGenerator>();
+        if (filesystem.Verify())
+        {
+            int amountToMint = configuration.GetValue<int>("Generation:SerieCount");
+            if (amountToMint == 0)
+            {
+                logger.LogWarning("Nothing to generate, amount to mint is set to 0");
+            }
+            else if (amountToMint < 0)
+            {
+                logger.LogError("Negative amount to mint (" + amountToMint + ") in config file");
+            }
+            else
+            {
+                generator.Generate();
+                await Task.Delay(250);
+                ConsoleExtensions.ClearConsoleLine();
+            }
+        }
+    }
+
+    public static async Task QueryResults(ArgumentsHandler handler, IServiceProvider services, ILogger logger)
+    {
+        IFilesystem filesystem = services.GetService<IFilesystem>();
+        IConfiguration configuration = services.GetService<IConfiguration>();
+        if (!Directory.Exists(Paths.RESULTS))
+        {
+            logger.LogWarning("There are no generated results");
+            return;
+        }
+        if (!Directory.Exists($"{Paths.RESULTS}rarities"))
+        {
+            logger.LogWarning("There are no generated rarities");
+            return;
+        }
+        var files = Directory.GetFiles($"{Paths.RESULTS}rarities", "*.json", SearchOption.AllDirectories);
+        logger.LogInformation($"{Paths.RESULTS}rarities");
+        if (files.Length != configuration.GetValue<int>("Generation:SerieCount"))
+        {
+            logger.LogWarning("The number of generated rarities differs from the serie amount");
+            return;
+        }
+        if (!filesystem.Verify())
+        {
+            return;
+        }
+        List<RarityMetadata> matches = new List<RarityMetadata>();
+        string pattern = handler.GetPositional(0);
+        if (pattern.ToLower().Equals("max"))
+        {
+            RarityMetadata maxRarityMetadata = null;
+            foreach (var file in files)
+            {
+                if (Serializer.DeserializeJson<RarityMetadata>(string.Empty, file, out var rarityMetadata))
+                {
+                    if (maxRarityMetadata is null || maxRarityMetadata.Rarity < rarityMetadata.Rarity)
+                    {
+                        maxRarityMetadata = rarityMetadata;
+                    }
+                }
+            }
+            logger.LogInformation("Max\n{max}", maxRarityMetadata.ToString());
+            return;
+        }
+        else if (pattern.ToLower().Equals("min"))
+        {
+            RarityMetadata minRarityMetadata = null;
+            foreach (var file in files)
+            {
+                if (Serializer.DeserializeJson<RarityMetadata>(string.Empty, file, out var rarityMetadata))
+                {
+                    if (minRarityMetadata is null || minRarityMetadata.Rarity > rarityMetadata.Rarity)
+                    {
+                        minRarityMetadata = rarityMetadata;
+                    }
+                }
+            }
+            logger.LogInformation("Min\n{min}", minRarityMetadata.ToString());
+            return;
+        }
+        var splits = pattern.Split('|');
+        if (splits.Length <= 0)
+        {
+            logger.LogWarning("No pattern provided");
+            return;
+        }
+        (int, int)[] layers = new (int, int)[splits.Length];
+
+        for (var i = 0; i < splits.Length; i++)
+        {
+            var split = splits[i];
+            var elem = split.Split(",");
+            if (!int.TryParse(elem[0], out var first) || !int.TryParse(elem[1], out var second))
+            {
+                logger.LogWarning("Unable to parse integers in pattern");
+                return;
+            }
+            if (first <= 0 || first >= filesystem.Layers.Count)
+            {
+                logger.LogError("Wrong query parameters");
+                return;
+            }
+            layers[i] = (first, second);
+        }
+
+        foreach (var file in files)
+        {
+            if (Serializer.DeserializeJson<RarityMetadata>(string.Empty, file, out var rarityMetadata))
+            {
+                bool isMatch = true;
+                foreach (var pair in layers)
+                {
+                    //logger.LogInformation("{1}, {2}", pair.Item1, pair.Item2);
+                    if (rarityMetadata.Hash[pair.Item1] != pair.Item2)
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch)
+                {
+                    matches.Add(rarityMetadata);
+                }
+            }
+        }
+        logger.LogInformation("Found {amount} matches", matches.Count);
+
+        if (handler.HasFlag("/p"))
+        {
+            matches.Sort((m1, m2) => m1.Id.CompareTo(m2.Id));
+            StringBuilder builder = new StringBuilder();
+            foreach (var pair in matches)
+            {
+                builder.Append($"Id: {pair.Id}{Environment.NewLine}");
+            }
+            logger.LogInformation(builder.ToString());
+        }
+    }
+
+    public static void PurgeRecursive(string path, ILogger logger = null)
+    {
+        var amount = 0;
+        DirectoryInfo dInfo = new DirectoryInfo(path);
+        foreach (FileInfo file in dInfo.EnumerateFiles())
+        {
+            amount++;
+            file.Delete();
+        }
+        logger?.LogInformation($"Deleted {amount} files");
+        amount = 0;
+        foreach (DirectoryInfo dir in dInfo.EnumerateDirectories())
+        {
+            amount++;
+            dir.Delete(true);
+        }
+        logger?.LogInformation($"Deleted {amount} directories");
+    }
+}
---
+++ NFTGenerator/Statics/Media.cs
@@ -0,0 +1,249 @@
+﻿// Copyright Matteo Beltrame
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace NFTGenerator;
+
+internal static class Media
+{
+    public enum Filter
+    {
+        VideoDegradation,
+        RgbShift
+    }
+
+    public static void ComposePNG(string res, ILogger logger, Filter? filter = null, params string[] mediaProvider)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        stopwatch.Restart();
+        Bitmap[] maps = new Bitmap[mediaProvider.Length];
+        for (int i = 0; i < maps.Length; i++)
+        {
+            string path = mediaProvider[i];
+            //logger.LogInfo(mediaProvider[i].ProvideMediaPath());
+            maps[i] = new Bitmap(path);
+        }
+        stopwatch.Stop();
+        //logger.LogInfo($"Created bitmaps {stopwatch.ElapsedMilliseconds} ms", ConsoleColor.Magenta);
+        stopwatch.Restart();
+        var width = maps[0].Width;
+        var height = maps[0].Height;
+        var target = new Bitmap(width, height);
+        using var composed = Graphics.FromImage(target);
+        Rectangle rect = new Rectangle(0, 0, width, height);
+        stopwatch.Stop();
+        //logger.LogInfo($"Created graphic {stopwatch.ElapsedMilliseconds} ms", ConsoleColor.Magenta);
+        stopwatch.Restart();
+        for (int i = 0; i < maps.Length; i++)
+        {
+            composed.DrawImage(maps[i], rect);
+            maps[i].Dispose();
+        }
+        stopwatch.Stop();
+        // logger.LogInfo($"Merges bitmaps {stopwatch.ElapsedMilliseconds} ms", ConsoleColor.Magenta);
+        stopwatch.Restart();
+        ApplyFilter(target, filter);
+        //var quantizer = new WuQuantizer();
+        //using var quantized = quantizer.QuantizeImage(target);
+        //quantized.Save(res, ImageFormat.Png);
+        target.Save(res, ImageFormat.Png);
+        target.Dispose();
+        composed.Dispose();
+        stopwatch.Stop();
+        //logger.LogInfo($"Finishing {stopwatch.ElapsedMilliseconds} ms", ConsoleColor.Magenta);
+    }
+
+    public static Bitmap ApplyFilter(Bitmap bitmap, Filter? filter)
+    {
+        if (filter == null) return null;
+        switch (filter)
+        {
+            case Filter.VideoDegradation:
+                return ApplyVideoDegradationFilter(bitmap);
+
+            case Filter.RgbShift:
+                return ApplyGlitchFilter(bitmap);
+        }
+        return null;
+    }
+
+    //horizontal shift
+
+    public static Bitmap Shift(Bitmap image, int lower, int upper, int shiftLower, int shiftUpper)
+    {
+        Bitmap test = new Bitmap(image);
+
+        Random r = new Random(image.GetHashCode());
+        int shift;
+
+        if (r.Next(0, 2) == 0)
+        {
+            shift = r.Next(shiftLower, shiftUpper);
+        }
+        else
+        {
+            shift = r.Next(1000 - shiftUpper, 1000 - shiftLower);
+        }
+
+        int h = r.Next(lower, upper);
+        int w = image.Width;
+        int position = r.Next(0, 1000);
+        Bitmap b = new Bitmap(w, h);
+        for (int i = 0; i < h; i++)
+        {
+            if (i + position >= image.Height)
+            {
+                break;
+            }
+            for (int j = 0; j < w; j++)
+            {
+                Color c = image.GetPixel(j, i + position);
+                if (j + shift < image.Width)
+                {
+                    test.SetPixel(j + shift, i + position, c);
+                }
+                else
+                {
+                    test.SetPixel(j + shift - image.Width, i + position, c);
+                }
+            }
+        }
+
+        return test;
+    }
+
+    private static Bitmap ApplyVideoDegradationFilter(Bitmap bitmap, int strenght = 15, int width = 3)
+    {
+        Color[] filters = new Color[] { Color.FromArgb(strenght, 0, 0), Color.FromArgb(0, strenght, 0), Color.FromArgb(0, 0, strenght) };
+        int filtersIndex = 0;
+        int currentIndex = 0;
+        for (int i = 0; i < bitmap.Height; i++)
+        {
+            Color currentFilter = filters[filtersIndex];
+            for (int j = 0; j < bitmap.Width; j++)
+            {
+                Color pixel = bitmap.GetPixel(j, i);
+                bitmap.SetPixel(j, i, Color.FromArgb(pixel.A,
+                    Math.Clamp(pixel.R + currentFilter.R, 0, 255),
+                    Math.Clamp(pixel.G + currentFilter.G, 0, 255),
+                    Math.Clamp(pixel.B + currentFilter.B, 0, 255)));
+            }
+
+            if (currentIndex == width)
+            {
+                currentIndex = 0;
+                filtersIndex = filtersIndex + 1 >= filters.Length ? 0 : filtersIndex + 1;
+            }
+            currentIndex++;
+        }
+        return bitmap;
+    }
+
+    private static Bitmap ApplyGlitchFilter(Bitmap image)
+    {
+        Random r = new Random(image.GetHashCode());
+        int shiftAmount = r.Next(5, 15);
+        Bitmap res = new Bitmap(1000, 1000);
+        //base blue layer
+        BluShift(image, res, shiftAmount);
+        //green shifted layer
+        GreenShift(image, res, shiftAmount);
+        //red shifted layer
+        RedShift(image, res, shiftAmount);
+        //horizontal shift lines
+        int lines = r.Next(8, 8);
+        for (int i = 0; i < lines; i++)
+        {
+            res = Shift(res, 30, 50, 20, 60);
+        }
+        return res;
+    }
+
+    #region Color Shift
+
+    private static void BluShift(Bitmap image, Bitmap res, int shiftAmount)
+    {
+        for (int i = 0; i < image.Height; i++)
+        {
+            for (int j = 0; j < image.Width; j++)
+            {
+                Color pixel = image.GetPixel(j, i);
+
+                res.SetPixel(j, i, Color.FromArgb(pixel.A, 0, 0, pixel.B));
+            }
+        }
+    }
+
+    private static Bitmap GreenShift(Bitmap image, Bitmap res, int shiftAmount)
+    {
+        Color pixel1;
+        for (int i = 0; i < image.Height; i++)
+        {
+            for (int j = 0; j < image.Width; j++)
+            {
+                Color pixel = image.GetPixel(j, i);
+
+                if (i + shiftAmount < image.Height && j + shiftAmount < image.Width)
+                {
+                    pixel1 = image.GetPixel(j + shiftAmount, i + shiftAmount);
+                    res.SetPixel(j + shiftAmount, i + shiftAmount, Color.FromArgb(pixel.A, 0, pixel1.G, pixel.B));
+                }
+                else if (j + shiftAmount >= image.Width && i + shiftAmount < image.Width)
+                {
+                    pixel1 = image.GetPixel(j, i + shiftAmount);
+                    res.SetPixel(j + shiftAmount - image.Width, i + shiftAmount, Color.FromArgb(pixel.A, 0, pixel1.G, pixel.B));
+                }
+                else if (j + shiftAmount < image.Width && i + shiftAmount >= image.Width)
+                {
+                    pixel1 = image.GetPixel(j + shiftAmount, i);
+                    res.SetPixel(j + shiftAmount, i + shiftAmount - image.Width, Color.FromArgb(pixel.A, 0, pixel1.G, pixel.B));
+                }
+                else if (j + shiftAmount >= image.Width && i + shiftAmount >= image.Width)
+                {
+                    res.SetPixel(j + shiftAmount - image.Width, i + shiftAmount - image.Width, Color.FromArgb(pixel.A, 0, pixel.G, pixel.B));
+                }
+            }
+        }
+        return res;
+    }
+
+    private static Bitmap RedShift(Bitmap image, Bitmap res, int shiftAmount)
+    {
+        Color pixel1;
+        for (int i = 0; i < image.Height; i++)
+        {
+            for (int j = 0; j < image.Width; j++)
+            {
+                Color pixel = res.GetPixel(j, i);//blue + green
+                if (i + shiftAmount < image.Width && j + shiftAmount < image.Width)
+                {
+                    pixel1 = image.GetPixel(j + shiftAmount, i + shiftAmount); //red
+                    res.SetPixel(j, i, Color.FromArgb(pixel.A, pixel1.R, pixel.G, pixel.B));
+                }
+                else if (i + shiftAmount < image.Width && j + shiftAmount >= image.Width)
+                {
+                    pixel1 = image.GetPixel(j + shiftAmount - image.Width, i + shiftAmount);
+                    res.SetPixel(j, i, Color.FromArgb(pixel1.A, pixel1.R, pixel.G, pixel.B));
+                }
+                else if (i + shiftAmount >= image.Width && j + shiftAmount < image.Width)
+                {
+                    pixel1 = image.GetPixel(j + shiftAmount, i + shiftAmount - image.Width);
+
+                    res.SetPixel(j, i, Color.FromArgb(pixel1.A, pixel1.R, pixel.G, pixel.B));
+                }
+                else if (i + shiftAmount >= image.Width && j + shiftAmount >= image.Width)
+                {
+                    pixel1 = image.GetPixel(j + shiftAmount - image.Width, i + shiftAmount - image.Width);
+                    res.SetPixel(j, i, Color.FromArgb(pixel1.A, pixel1.R, pixel.G, pixel.B));
+                }
+            }
+        }
+        return res;
+    }
+
+    #endregion Color Shift
+}
